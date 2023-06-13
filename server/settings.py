ef0a940b5f6e5d706da9f7f3a22e4c7c46ac70f4
Katerina Hanson <katerina.hanson@gmail.com>
2023-06-12 23:25:28 -0700

    Pull prod env from s3 (#13)

--- server/settings.py
+++ server/settings.py
@@ -20,17 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-ENV = os.environ['environment']
 # SECURITY WARNING: keep the secret key used in production secret!
-if ENV != 'local':
-    SECRET_KEY = os.environ['SECRET_KEY']
-    ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
-else:
-    SECRET_KEY = 'django-insecure-7bh9ydf&0v3qb2t_69iv!rxn*t$*@*n!w1#5zdmi!zke2p%=o_'
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7bh9ydf&0v3qb2t_69iv!rxn*t$*@*n!w1#5zdmi!zke2p%=o_')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', ['localhost', '127.0.0.1'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 
 
@@ -86,10 +82,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'restaurant',
-        'USER': 'postgres',
-        'PASSWORD': 'mys3cretpassw0rd!',
-        'HOST': 'db',
-        'PORT': 5432,
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'mys3cretpassw0rd!'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
 
