770cf20495b8339e1e792514f3f010736461112e
Katerina Hanson <katerina.hanson@gmail.com>
2023-07-02 20:06:03 -0700

    Fixing logged in homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../auth/AuthContext'
-import { HorizontalNavBar } from '../components'
+import React from 'react'
+// import { AuthContext } from '../auth/AuthContext'
+import { HorizontalNavBar, Logo } from '../components'
+import { Row, Col } from 'antd'
 
 const HomePage = () => {
-  const { logOut } = useContext(AuthContext)
+  // const { logOut } = useContext(AuthContext)
 
   const items = [
     {
@@ -22,9 +23,24 @@ const HomePage = () => {
 
   return (
     <div>
-      <HorizontalNavBar items={items} />
-      You are logged in
-      <button onClick={logOut}>Log Out</button>
+      <Row>
+        <Col span={8}>
+          <Logo />
+        </Col>
+        <Col span={16} />
+      </Row>
+      <Row>
+        <Col span={2} />
+        <Col span={20}>
+          <HorizontalNavBar items={items} />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={2} />
+        <Col span={20}>
+          <p>You are logged in</p>
+        </Col>
+      </Row>
     </div>
   )
 }
