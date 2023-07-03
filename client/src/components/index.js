d7573cd191ab64bb2c2d570df3c8c37c5649e403
Katerina Hanson <katerina.hanson@gmail.com>
2023-07-02 19:14:52 -0700

    Styling the login and registration pages

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+// import components
+import Button from './Button'
+import Input from './Input'
+// import Logo from './Logo';
+
+// re-export components
+// export { Button, Input, Logo };
+export { Input, Button }
